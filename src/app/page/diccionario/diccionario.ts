c281161c047bea25f3c7c2830ffe324ac3e2c7ca
David <davidaar05@gmail.com>
2026-02-05 17:14:23 -0500

    primer commitxd

---
+++ src/app/page/diccionario/diccionario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diccionario',
+  imports: [],
+  templateUrl: './diccionario.html',
+  styleUrl: './diccionario.css',
+})
+export class Diccionario {
+
+}
---
+++ src/app/page/yume-nikki/yume-nikki.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yume-nikki',
+  imports: [],
+  templateUrl: './yume-nikki.html',
+  styleUrl: './yume-nikki.css',
+})
+export class YumeNikki {
+
+}
