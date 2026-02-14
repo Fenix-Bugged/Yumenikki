9198b2ff29db82c65571cced4dc14239f94baf97
Tu Nombre <tu@email.com>
2026-02-14 12:37:00 -0500

    muchos avances xdxd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './page/inicio/inicio';
+import { QueEs } from './page/que-es/que-es';
+import { SobreMi } from './page/sobre-mi/sobre-mi';
+import { YumeNikki } from './page/yume-nikki/yume-nikki';
+import { Diccionario } from './page/diccionario/diccionario';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component:Inicio},
+    {path: 'que-es',component:QueEs},
+    {path: 'sobre-mi',component:SobreMi},
+    {path: 'yume-nikki',component:YumeNikki},
+    {path: 'diccionario',component:Diccionario},
+
+];
